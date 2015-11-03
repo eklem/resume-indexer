6e8b8f72d6695eaa36d734473ba4b771cf2477f2
Espen Klem <espen.klem@gmail.com>
2015-11-03 23:51:38 +0100

    Removed old crap. Added indexer for each type of results. Not ideal, but working good. Added relevancy weight for people to get me at top (best bet kind of).

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Life-Indexer
-The indexing module for Life-Index. Pulls JSON from Google Drive Spreadsheets with [Gsheets](https://github.com/interactivethings/gsheets), document process it and index it with Search-Index si.add. The data in Google Drive Spreadsheets are from different sources, added by IFTTT.com.
+# resume-indexer
+An indexer for the search driven CV / resumé: resume-index
---
+++ indexer-organizations.js
@@ -0,0 +1,68 @@
+// Modules and stuff required
+var gsheets = require('gsheets')
+var fs = require('fs-extra')
+var ifttnorch = require('iftt-norch-tools')
+var options = {
+    indexPath: 'resume-si',
+    logLevel: 'info',
+    logSilent: false,
+    nGramLength: 3
+}
+var si = require('search-index')(options)
+var jf = require('jsonfile')
+var util = require('util')
+var configfile = '/Users/eklem/node_modules/resume-indexer/config/config-resume-organizations.json'
+
+
+// Read config file
+var config = jf.readFileSync(configfile);
+//console.dir(config.categories);
+
+// Get csv-file as 'data' (object)
+gsheets.getWorksheet(config.gsheetsKey, config.categories, function (err, result) {
+    if (err) {
+        console.dir(err);
+    }
+    //console.dir(result.data);
+    // Check if ANY changes since last indexing process
+    if (result.updated != config.gsheetLastUpdated) {
+
+        //console.log('Index is not up to date.\nGsheet updated: ' + config.gsheetLastUpdated + '\nConfig updated: ' + result.updated)
+        var newItems = []
+        var datesUpdated = []
+
+        // Iterating through rows of data from spreadsheet
+        for (var j = 0; j < result.data.length; j++) {
+            var obj = result.data[j]
+            // Document processing the rest
+            obj.id = ifttnorch.id(obj.categories + obj.title);
+            obj.categories = [obj.categories];
+            obj.types = ifttnorch.tagslist(obj.types);
+            obj.tags = ifttnorch.tagslist(obj.tags);
+            obj.teasertext = ifttnorch.sanitizehtml(obj.text, [], {});
+            // Push to the array that will be indexed + array for latest update
+            newItems.push(obj)
+            datesUpdated.push(obj.date)
+        }
+
+        //Index newItems and update config-file with new dates
+        si.add(newItems, {
+            'batchName': config.categories,
+            fieldOptions: [
+               {fieldName: 'categories', filter: true},
+               {fieldName: 'types', filter: true},
+               {fieldName: 'tags', filter: true}
+            ]
+
+        }, function (err) {
+            if (!err) {
+                console.log('Indexed!')
+                //config.gsheetLastUpdated = result.updated
+                //console.dir(config)
+
+                // Write config file
+                //jf.writeFileSync(configfile, config)
+            }
+        });
+    };
+});
